d0be3c7549535e0737a37da36f081a497a6aa584
Nikitulka0120 <107490259+Nikitulka0120@users.noreply.github.com>
2025-10-05 16:35:22 +0700

    methods in class

--- files.cpp
+++ files.cpp
@@ -3,8 +3,7 @@
 #include <cstring>
 using namespace std;
 
-struct BuyTea
-{
+struct BuyTea {
     char TeaType[50];
     char PackForm[50];
     char Manufacturer[50];
@@ -30,7 +29,7 @@ struct BuyTea
         sum = price * amount;
     }
 
-    void print() {
+    void print() const {
         cout << "Тип: " << TeaType << endl;
         cout << "Форма упаковки: " << PackForm << endl;
         cout << "Производитель: " << Manufacturer << endl;
@@ -41,96 +40,128 @@ struct BuyTea
     }
 };
 
-void bubbleSort(BuyTea* arr, int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j].sum > arr[j + 1].sum) {
-                BuyTea temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+class TeaFileManager {
+private:
+    BuyTea* teas = nullptr;
+    int count = 0;
+    float totalSum = 0;
+
+public:
+    bool readFromText(const char* filename) {
+        ifstream f(filename);
+        if (!f.is_open()) {
+            cout << "Ошибка открытия файла: " << filename << endl;
+            return false;
+        }
+
+        f >> count;
+        f.ignore();
+
+        delete[] teas;
+        teas = new BuyTea[count];
+
+        for (int i = 0; i < count; i++) {
+            string teaType, packForm, manufacturer;
+            float price;
+            int amount;
+
+            getline(f, teaType);
+            getline(f, packForm);
+            getline(f, manufacturer);
+            f >> price >> amount;
+            f.ignore();
+
+            teas[i] = BuyTea(teaType.c_str(), packForm.c_str(), manufacturer.c_str(), price, amount);
+        }
+
+        f.close();
+        cout << "Считано " << count << " записей из текстового файла\n" << endl;
+        return true;
+    }
+
+    void sortBySum() {
+        for (int i = 0; i < count - 1; i++) {
+            for (int j = 0; j < count - i - 1; j++) {
+                if (teas[j].sum > teas[j + 1].sum) {
+                    BuyTea temp = teas[j];
+                    teas[j] = teas[j + 1];
+                    teas[j + 1] = temp;
+                }
             }
         }
     }
-}
 
-int main()
-{
+    bool writeToBinary(const char* filename) {
+        ofstream bout(filename, ios::binary);
+        if (!bout.is_open()) {
+            cout << "Ошибка открытия бинарного файла: " << filename << endl;
+            return false;
+        }
+
+        bout.write((char*)&count, sizeof(count));
+
+        totalSum = 0;
+        for (int i = 0; i < count; i++) {
+            bout.write((char*)&teas[i], sizeof(BuyTea));
+            totalSum += teas[i].sum;
+        }
+
+        bout.write((char*)&totalSum, sizeof(totalSum));
+        bout.close();
+
+        cout << "Данные записаны в бинарный файл: " << filename << "\n" << endl;
+        return true;
+    }
+
+    bool readFromBinary(const char* filename) {
+        ifstream bin(filename, ios::binary);
+        if (!bin.is_open()) {
+            cout << "Ошибка открытия бинарного файла: " << filename << endl;
+            return false;
+        }
+
+        bin.read((char*)&count, sizeof(count));
+
+        delete[] teas;
+        teas = new BuyTea[count];
+
+        for (int i = 0; i < count; i++) {
+            bin.read((char*)&teas[i], sizeof(BuyTea));
+        }
+
+        bin.read((char*)&totalSum, sizeof(totalSum));
+        bin.close();
+
+        cout << "Считано " << count << " записей из бинарного файла\n" << endl;
+        return true;
+    }
+
+    void printAll() const {
+        for (int i = 0; i < count; i++) {
+            teas[i].print();
+        }
+        cout << "Общая сумма: " << totalSum << endl;
+    }
+};
+
+int main() {
     system("chcp 65001 > nul");
 
-    ifstream f("firstinput.txt");
-    if (!f.is_open()) {
-        cout << "Ошибка открытия файла!" << endl;
+    TeaFileManager manager;
+
+    if (!manager.readFromText("firstinput.txt"))
         return 1;
-    }
 
-    int records_count;
-    f >> records_count;
-    f.ignore();
+    manager.sortBySum();
+    manager.printAll();
 
-    BuyTea* allteas = new BuyTea[records_count];
-
-    for (int i = 0; i < records_count; i++) {
-        string teaType, packForm, manufacturer;
-        float price;
-        int amount;
-        getline(f, teaType);
-        getline(f, packForm);
-        getline(f, manufacturer);
-        f >> price >> amount;
-        f.ignore();
-        allteas[i] = BuyTea(teaType.c_str(), packForm.c_str(), manufacturer.c_str(), price, amount);
-    }
-    f.close();
-
-    cout << "Считано " << records_count << " записей из текстового файла:\n" << endl;
-    for (int i = 0; i < records_count; i++)
-        allteas[i].print();
-
-    bubbleSort(allteas, records_count);
-
-    ofstream bout("bin.dat", ios::binary);
-    if (!bout.is_open()) {
-        cout << "Ошибка открытия бинарного файла!" << endl;
+    if (!manager.writeToBinary("bin.dat"))
         return 2;
-    }
 
-    bout.write((char*)&records_count, sizeof(records_count));
-
-    float totalSum = 0;
-    for (int i = 0; i < records_count; i++) {
-        bout.write((char*)&allteas[i], sizeof(BuyTea));
-        totalSum += allteas[i].sum;
-    }
-    bout.write((char*)&totalSum, sizeof(totalSum));
-    bout.close();
-    cout << "Все записи сохранены в бинарный файл bin.dat\n" << endl;
-
-    ifstream bin("bin.dat", ios::binary);
-    if (!bin.is_open()) {
-        cout << "Ошибка открытия бинарного файла для чтения!" << endl;
+    TeaFileManager binManager;
+    if (!binManager.readFromBinary("bin.dat"))
         return 3;
-    }
 
-    int read_count;
-    bin.read((char*)&read_count, sizeof(read_count));
-    BuyTea* binTeas = new BuyTea[read_count];
-
-    for (int i = 0; i < read_count; i++) {
-        bin.read((char*)&binTeas[i], sizeof(BuyTea));
-    }
-
-    float read_totalSum;
-    bin.read((char*)&read_totalSum, sizeof(read_totalSum));
-    bin.close();
-
-    cout << "Считано " << read_count << " записей из бинарного файла:\n" << endl;
-    for (int i = 0; i < read_count; i++)
-        binTeas[i].print();
-
-    cout << "Всего: " << read_totalSum << endl;
-
-    delete[] allteas;
-    delete[] binTeas;
-
+    binManager.printAll();
     return 0;
 }
